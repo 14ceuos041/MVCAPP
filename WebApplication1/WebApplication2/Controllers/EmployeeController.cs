6f84071e30c895acf6523054b6f509cfe965a510
baxi099 <baxi099@gmail.com>
2016-09-20 22:50:22 +0530

    Added MVC

---
+++ WebApplication1/WebApplication2/Controllers/EmployeeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        //
+        // GET: /Employee/
+        public ActionResult Show(string firstname)
+        {
+            if (string.IsNullOrEmpty(firstname))
+            {
+                ViewData["ErrorMessage"] = "No firstname provided!";
+            }
+            else
+            {
+                Employee employee = new Employee
+                {
+                    FirstName = firstname,
+                    LastName = "Example",
+                    Email = firstname + "@example.com"
+                };
+
+                ViewData["FirstName"] = employee.FirstName;
+                ViewData["LastName"] = employee.LastName;
+                ViewData["Email"] = employee.Email;
+            }
+            return View();
+
+        }
+    }
+}
